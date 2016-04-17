2e49b2d8d63423a46e471140391251a4d9b657e0
erindepew <erindepew@gmail.com>
2016-04-17 09:19:02 -0400

    adding in stuff to work page

--- app/Work.jsx
+++ app/Work.jsx
@@ -1,11 +1,16 @@
 var React = require('react');
 var Thumbnail = require('./Thumbnail.jsx');
+var Navigation = require('./Navigation.jsx');
 
 var Work = React.createClass({
   render: function () {
+    var layoutClass = this.props.navState ? 'layout--wrapper open' : 'layout--wrapper closed';
     return (
-      <div className="layout--MAIN">
-        <Thumbnail text="label text"/>
+      <div className='layout--MAIN'>
+        <div className={layoutClass}>
+          <Thumbnail text='text'/>
+        </div>
+        <Navigation navState={this.props.navState}/>
       </div>
     );
   }
