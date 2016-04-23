2c9d724ba60bb5a14044f13551d373ce1180440b
erindepew <erindepew@gmail.com>
2016-04-23 16:59:37 -0400

    more changes

--- app/Thumbnail.jsx
+++ app/Thumbnail.jsx
@@ -4,7 +4,10 @@ var Thumbnail = React.createClass({
   render: function () {
     return (
       <div className="thumbnail--MAIN">
-        <div className="thumbnail--overlay">{this.props.text}</div>
+        <div className="thumbnail--overlay">
+          <h5 className='thumbnail--title'>{this.props.title}</h5>
+          <p className='thumbnail--text'>{this.props.text}</p>
+        </div>
         <img className='thumbnail--image' src={`images/${this.props.imageUrl}`}/>
       </div>
     );
--- app/Work.jsx
+++ app/Work.jsx
@@ -4,7 +4,9 @@ var Navigation = require('./Navigation.jsx');
 var Filter = require('./Filter.jsx');
 
 const filterOptions = ['Photos', 'Code', 'Design', 'UX'];
-const design = [{image:'sunflower.jpg', text:'text content'}, {image:'sunflower.jpg', text:'text content'}, {image:'sunflower.jpg', text:'text content'}];
+const design = [{image:'sunflower.jpg', text:'text content', title: 'title for thing'},
+  {image:'sunflower.jpg', text:'text content', title: 'title for thing'},
+  {image:'sunflower.jpg', text:'text content', title: 'title for thing'}];
 
 var Work = React.createClass({
   render: function () {
@@ -13,7 +15,7 @@ var Work = React.createClass({
         <div className={`layout--wrapper ${this.props.navState ? 'layout--wrapper open' : 'layout--wrapper closed'}`}>
           <Filter label='Filter by' buttons={filterOptions}/>
           {design.map(function (item) {
-            return <Thumbnail text={item.text} imageUrl={`portfolio/${item.image}`}/>;
+            return <Thumbnail text={item.text} title={item.title} imageUrl={`portfolio/${item.image}` }/>;
           })}
         </div>
         {/*<Navigation navState={this.props.navState}/>*/}
