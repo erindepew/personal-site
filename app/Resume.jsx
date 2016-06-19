5c1edd7cc4f4a013aad49700fd71bbd11f05063c
erindepew <erindepew@gmail.com>
2016-06-19 09:09:40 -0400

    adding in content to resume

--- app/Resume.jsx
+++ app/Resume.jsx
@@ -7,27 +7,17 @@ var Navigation = require ('./Navigation.jsx');
 var experience = [
   {
     header: 'Frontend Developer',
-    subheader: 'Bitly, 2015 - present',
-    content: 'brief description'
+    subheader: 'Bitly, February 2015 - present',
+    content: '• Project lead for re-building website as a full-fledged SaaS platform using React, Omniscient, and Immutable using functional programming techniques. • Collaborated with product, design and data-science team to develop features and data visualizations for customers.• Maintained features on current Backbone and Marionette site while new platform is under construction.• Built and maintained internal ‘live’ styleguide which is used for collaborating with design and UX team on SaaS platform.• Developed process for creating CSS modules for use with React components which increased iteration speed and decreased total number of lines of CSS by 50%'
   },
   {
     header: 'Frontend Design Developer',
-    subheader: 'NBCUniversal, 2013 - 2015',
-    content: 'brief description'
-  },
-  {
-    header: 'Freelance Designer and Developer',
-    subheader: '2012 - present',
-    content: 'brief description'
-  },
-  {
-    header: 'Curator',
-    subheader: 'Swirl Network, 2011 - 2012',
-    content: 'brief description'
+    subheader: 'NBCUniversal, March 2013 - February 2015',
+    content: '• Developed responsive components and layouts using HTML5 and Sass for the Telemundo.com redesign with the Telemundo designer team and the NBC development team.• Developed a process to integrate the visual designer’s and front-end engineer’sworkflow, focusing on pair-programming web applications in-browser. Served as a bridge between the graphic designers and engineers, enabling the process to become designer and developer driven (agile-like) and drastically increasing efficiency.• Rapidly prototyped proof-of-concept systems using HTML5, CSS3, andJavaScript to serve as examples to both the UX and design teams.• Designed assets such as ads, headers, and icons for websites, signage,and mobile apps.'
   }
 ];
 
-var skills = ['JavaScript, CSS3, HTML5, Sass, ES6, React, Omniscient, Immutable', 'Illustrator, Photoshop, Indesign', 'Wordpress, Drupal', 'Python, PHP', 'git, sh, grep, awk, webstorm', 'MySQL'];
+var skills = ['JavaScript, CSS3, HTML5, Sass, ES6, React, Omniscient, Immutable', 'Illustrator, Photoshop, Indesign', 'Wordpress, Drupal', 'Python, PHP', 'git, grep, awk', 'MySQL'];
 
 var contact = [ 'erinldepew@gmail.com', '@erindepew', 'github.com/erindepew', 'linkedin.com/erindepew'];
 
